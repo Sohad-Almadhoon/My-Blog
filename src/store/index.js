0ebdcfbe7cc2151264021c8e12346469c7d3f4b9
Sohad-Almadhoon <89332592+Sohad-Almadhoon@users.noreply.github.com>
2022-08-30 08:50:26 -0700

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { LogIn } from "../reducer/LogIn";
 import { createBlog } from '../reducer/CreateBlog';
-import { localStorageMiddleWare, retriveBlog } from "../middleware";
+import { retriveBlog } from "../middleware";
 export const store = configureStore({
   reducer: { auth: LogIn.reducer, blog: createBlog.reducer },
   preloadedState:retriveBlog(),
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(localStorageMiddleWare),
 });
